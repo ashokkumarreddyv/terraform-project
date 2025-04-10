29d11dc134279c94cafa7e692cd2dfe8e5cdfadc
ashokv <ashok3akr@gmail.com>
2025-04-10 17:30:58 +0530

    ansible

--- frontend.tf
+++ frontend.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = [sg-07b6cb1ebb35644ea]
+  vpc_security_group_ids = ["sg-07b6cb1ebb35644ea"]
 
   tags = {
     Name = "frontend"
