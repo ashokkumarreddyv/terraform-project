29b53781c70a960b0cd7327bfe89ba0c5bcd6ca2
ashokv <ashok3akr@gmail.com>
2025-04-11 17:04:29 +0530

    ansible

--- frontend.tf
+++ frontend.tf
@@ -7,3 +7,19 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+provisioner "remote-exec" {
+ connection {
+  type     = "ssh"
+  user     = "ec2-user"
+  password = "DevOps321"
+  host     = self.public_ip
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "pip3.11 install ansible",
+    "ansible-pull -i localhost, -u https://github.com/ashokkumarreddyv/roboshop roboshop.yml -e component_name=frontend -e env=dev",
+  ]
+}
+}
