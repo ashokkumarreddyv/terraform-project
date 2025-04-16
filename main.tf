1e8202a88a07e9696150cb1403c5ed39bbe4000f
ashokv <ashok3akr@gmail.com>
2025-04-16 10:41:30 +0530

    ansible

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "instance" {
+  count = length(var.instances)
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+
+}
+
+# resource  "null_resource" "frontend" {
+#
+#   provisioner "remote-exec" {
+#
+#     connection {
+#       type     = "ssh"
+#       user     = "ec2-user"
+#       password = "DevOps321"
+#       host     = aws_instance.frontend.public_ip
+#     }
+#
+#
+#     inline = [
+#       "sudo pip3.11 install ansible",
+#       "ansible-pull -i localhost, -U https://github.com/ashokkumarreddyv/Ansible-project roboshop.yml -e component_name=mysql -e env=dev"
+#     ]
+#
+#   }
+#
+# }
+
+
+
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "ami_id" {
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-07b6cb1ebb35644ea"]
+}
+
+
+variable "instances" {
+  default = [
+  "frontend"
+  "Mongodb",
+  "catalogue",
+  "Redis",
+  "user"
+
+
+  ]
+
+}
