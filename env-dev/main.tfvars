b77903e21214aeda8f9fb6b11efadb135c70b819
ashokv <ashok3akr@gmail.com>
2025-04-21 10:54:18 +0530

    ansible

---
+++ env-dev/main.tfvars
@@ -0,0 +1,20 @@
+instances= {
+  frontend = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+  catalogue = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+  mangodb = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+  cart = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+}
+vpc_security_group_ids = ["sg-07b6cb1ebb35644ea"]
+env = "dev"
--- variables.tf
+++ variables.tf
@@ -1,36 +1,7 @@
 
-
-variable "vpc_security_group_ids" {
-  default = ["sg-07b6cb1ebb35644ea"]
-}
-
-
-variable "instances" {
-  default = {
-    frontend = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-    catalogue = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-    mangodb = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-    cart = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-  }
-
-
-}
-
-variable "env" {
-  default = "env"
-}
+variable "vpc_security_group_ids" {}
+variable "instances" {}
+variable "env" {}
 
 # variable "zone_id" {
 #   default = ""
